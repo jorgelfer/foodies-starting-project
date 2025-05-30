de408ff30c925d399ce9d93994f36b07716daafe
jorgelfer <jorgeferram17@gmail.com>
2025-05-30 11:48:06 -0500

    showing details for particular meals

--- app/meals/[MealSlug]/page.js
+++ app/meals/[MealSlug]/page.js
@@ -1,3 +1,40 @@
-export default function MealDetailPage() {
-  return <h1>Meal Detail</h1>;
+import { getMeal } from "@/lib/meals";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+// any dynamic path segment will be available in params
+export default function MealDetailPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  // show the closest not found page
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill></Image>
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
